f9284f12c7d7230eaf439105b9ff6132234dde3b
Nathanael Silverman <nathanael.silverman@hevenet.com>
2013-02-13 09:44:15 +0100

    Fixed step processing.

--- sequence.js
+++ sequence.js
@@ -2,15 +2,14 @@ function Sequence() {
   this.steps = [];
 
   this.append = function(step) {
-    this.process(step);
-    this.steps.push(step);
+    this.steps.push(this.process(step));
   };
 
   this.run = function() {
     if (!this.steps.length) return;
 
     var step = this.steps.shift(); // removes it from the array as well
-
+  
     window.setTimeout(_.bind(function() {
       if (step.execute() !== false) {
         this.run();
@@ -19,13 +18,13 @@ function Sequence() {
   };
 
   this.prepend = function(step) {
-    this.process(step);
-    this.steps.unshift(step);
+    this.steps.unshift(this.process(step));
   };
 
   this.process = function(step) {
     if (typeof step == 'function') step = { execute: step };
     step.delay = step.delay || 0;
     step.execute = step.execute || function(){};
+    return step;
   };
 };
